92de40039a560f20f9405cf57a0c57c0e40db1e2
Jean Santos <jeanvictor.san@gmail.com>
2025-04-21 21:49:44 -0300

    fix: Inclui anotation ignore para serialização

--- src/main/java/dev/java10x/CadastroDeNinja/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Missoes/MissoesModel.java
@@ -1,5 +1,6 @@
 package dev.java10x.CadastroDeNinja.Missoes;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.java10x.CadastroDeNinja.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -24,7 +25,7 @@ public class MissoesModel {
     private String dificuldade;
 
     @OneToMany(mappedBy = "missoes")
+    @JsonIgnore
     private List<NinjaModel> ninjas;
-
 
 }
