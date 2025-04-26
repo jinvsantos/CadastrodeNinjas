434f89f54583f855d64006c5b73c5244b9bfa3ad
Jean Santos <jeanvictor.san@gmail.com>
2025-04-26 01:21:39 -0300

    chore: Inclue a versao do DTO pela entidade e mapeia manualmente no NinjaMapper

--- src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaDTO.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaDTO.java
@@ -1,4 +1,21 @@
 package dev.java10x.CadastroDeNinja.Ninjas;
 
+import dev.java10x.CadastroDeNinja.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private int idade;
+    private String rank;
+    private MissoesModel missoes;
+
 }
